024f12bab6d80706da6aaf03937e37d7d2e23330
Chowdarychilukuri <chilukuridevops@gmail.com>
2024-05-14 11:02:55 +0530

    gitignore

---
+++ session1/EC2/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0f3c7d07486cad139" #devops-practice
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id]  #This means list
+
+  tags = {
+    Name = "HelloTerraform"
+  }
+}
+
+resource "aws_security_group" "roboshop-all" {    #This is terraform name, for terraform reference only
+    name        = "roboshop-aws-all"    #This is AWS name
+    description = "Allow TLS inbound traffic"
+    #vpc_id      = aws_vpc.main.id
+
+    ingress {
+        description      = "Allow All ports"
+        from_port        = 0 # 0 means all ports
+        to_port          = 0 
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        #ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "roboshop-all-aws"
+    }
+}
