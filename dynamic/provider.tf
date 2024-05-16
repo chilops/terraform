933bf62e672cd518129ad74d42e6a4a855a33789
Chowdarychilukuri <chilukuridevops@gmail.com>
2024-05-16 13:34:31 +0530

    terraform

---
+++ dynamic/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.49.0"  #AWS provider version, not terraform version
+    }
+  }
+
+  backend "s3" {
+    bucket = "chilops-terraform-remotestate"
+    key    = "dynamic"
+    region = "us-east-1"
+    dynamodb_table = "chilops-locking"
+  }
+}
+provider "aws" {
+     region = "us-east-1"
+}
